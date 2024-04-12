64cb5ffd372fb8185dc4384f684d685ae4431726
Ana Mejia <al222210590@gmail.com>
2024-04-12 11:03:16 -0600

    agregar cors

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const router = express.Router();
 app.use(express.json())
 app.use(cors())
 
+//------------------------------------------------------------------
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 // Let us run the server. SO its running,
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
