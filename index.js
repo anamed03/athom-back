61f717927b7b8d041a1949bb02334f931650d022
Ana Mejia <al222210590@gmail.com>
2024-04-15 11:00:21 -0600

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 //------------------------------------------------------------------
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
