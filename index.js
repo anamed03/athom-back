90d347408b9636c65664241f1a3f18e0d3bd1a70
Ana Mejia <al222210590@gmail.com>
2024-04-16 22:43:53 -0600

    local

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const cors = require('cors');
 const { PORT } = require('./config.js');
 const { connectDB } = require('./db.js');
 const { setupRoutes } = require('./routes.js');
-import { APP_URL } from './config.js';
 const app = express();
 
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: APP_URL
+    origin: 'APP_URL'
 }));
 
 // Middleware para configurar los encabezados CORS
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', APP_URL);
+    res.setHeader('Access-Control-Allow-Origin', 'APP_URL');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
