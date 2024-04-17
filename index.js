7d575d1645217edf8c8870fb3b366ca7b234e378
Ana Mejia <al222210590@gmail.com>
2024-04-16 22:16:50 -0600

    local

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'http://localhost'
 }));
 
 // Middleware para configurar los encabezados CORS
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
